d476a8fda5cda70f178addc602a1c2a55045bb08
hoonhur <66529909+hoonhur@users.noreply.github.com>
2020-07-06 19:44:11 -0400

    Start: Set up local Server
    Set up local server

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+var path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+app.use(bodyParser.urlencoded(false))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
