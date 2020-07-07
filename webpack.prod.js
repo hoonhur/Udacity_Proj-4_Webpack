5f2d52e7037ca55d5ad993d6c7e0ad66a4709d1b
hoonhur <66529909+hoonhur@users.noreply.github.com>
2020-07-06 21:40:17 -0400

    Followed to Lesson 2 - basic of webpack
    followed to L2 - basic set up of webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,44 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugIn = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 
 module.exports = {
+    mode: "production",
+    devtool: 'source-map',
     entry: './src/client/index.js',
-    mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
-        ]
+        ],
     },
     plugins: [
-        new HtmlWebPackPlugin({
+        new HtmlWebpackPlugIn({
             template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
+            filename: "./index.html"
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
     ]
+
 }
