be51ea2aa89afe721d8f558fa22da3fc5f1a15e3
hoonhur <66529909+hoonhur@users.noreply.github.com>
2020-07-19 11:09:28 -0400

    API request
    set up API call

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,63 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let url = document.getElementById('url').value
+    Client.checkForUrl(url)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+
+    postData('/api', {text: url})
+    .then(() => {
+        getSentiment('/projectData')
     })
+    .then(() => {
+        updateUI()
+    })
+}
+
+    const postData = async (url = '', data = {}) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        try {
+            const newData = await res.json()
+            return newData
+        } catch(error) {
+            console.log('error at post data', error)
+        }
+    }
+
+const getSentiment = async (url) => {
+    const res = await fetch ('/projectData')
+    try {
+        const data = await res.json()
+        return data
+    } catch(error) {
+        console.log('error at getSentiment', error)
+    }
+}
+
+const updateUI = async () => {
+    const req = await fetch('/sentiment')
+    try {
+        const allData = await req.json()
+        console.log(allData.polarity)
+        document.getElementById('polarity').innerHTML = allData.polarity;
+        document.getElementById('polarity_confidence').innerHTML = allData.polarity_confidence;
+        document.getElementById('subjectivity').innerHTML = allData.subjectivity;
+        document.getElementById('subjectivity_confidence').innerHTML = allData.subjectivity_confidence
+        document.getElementById('exerpt').innerHTML = allData.text;
+       
+        
+    } catch(error) {
+        console.log('error at updateUI', error)
+
+    }
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,60 @@
-var path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
 
+const app = express()
 app.use(express.static('dist'))
 app.use(bodyParser.urlencoded(false))
 app.use(bodyParser.json())
 app.use(cors())
 
 
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get('/', (req, res) => res.sendFile('dist/index.html'))
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+app.listen(8082, () => console.log('Example app listening on port 8082!'))
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+const Aylien = require('aylien_textapi')
+
+projectData = {}
+
+const textApi = new Aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+})
+
+const data = []
+
+app.post('/api', (req, res) => {
+    console.log("Request to 'api' endpoint", req.body.text)
+    data.push(req.body)
+    data['text'] = req.body.text
+    textApi.sentiment({ 
+        text: data.text,
+        mode: 'tweet'
+    }, function (error, response) {
+        if (error === null) {
+            projectData['polarity'] = response.polarity
+            projectData['subjectivity'] = response.subjectivity
+            projectData['text'] = response.text
+            projectData['polarity_confidence'] = response.polarity_confidence
+            projectData['subjectivity_confidence'] = response.subjectivity_confidence
+            res.send(projectData)
+            console.log(projectData)
+        } else {
+            console.log(error, 'error')
+        }
+        
+    })
+})
+
+app.get('/projectData', (req, res) => {
+    res.send(projectData)
 })
