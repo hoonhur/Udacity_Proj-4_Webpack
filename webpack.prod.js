559bf7478cfb9e5b294c58a98ab344c88e9c833c
hoonhur <66529909+hoonhur@users.noreply.github.com>
2020-07-19 23:39:38 -0400

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,44 +1,40 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugIn = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
-    mode: "production",
-    devtool: 'source-map',
     entry: './src/client/index.js',
+    mode: 'production',
     output: {
         libraryTarget: 'var',
         library: 'Client'
     },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
-        ],
+        ]
     },
     plugins: [
-        new HtmlWebpackPlugIn({
+        new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html"
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
+        new MiniCssExtractPlugin({filename: "[name].css"}),
+        new WorkboxPlugin.GenerateSW()
     ]
-
 }
