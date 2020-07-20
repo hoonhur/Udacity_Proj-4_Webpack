52b0b6e971dc77156e0439b191a42c4d0f687fef
hoonhur <66529909+hoonhur@users.noreply.github.com>
2020-07-20 00:03:25 -0400

    Add files via upload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugIn = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = new require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: "production",
-    devtool: 'source-map',
     entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    devServer: {
+        port: 8080
+    },
     output: {
         libraryTarget: 'var',
         library: 'Client'
@@ -15,20 +18,20 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
-        ],
+        ]
     },
     plugins: [
-        new HtmlWebpackPlugIn({
+        new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
-            filename: "./index.html"
+            filename: "./index.html",
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
@@ -38,8 +41,6 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        }),
-        //new BundleAnalyzerPlugin()
+        })
     ]
-
 }
